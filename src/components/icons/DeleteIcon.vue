80cf8008b673fa9f9cbd748fd4f6d2dd89543d8e
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2023-09-24 21:25:27 +0500

    icons size

--- src/components/icons/DeleteIcon.vue
+++ src/components/icons/DeleteIcon.vue
@@ -1,5 +1,5 @@
 <template>
-  <svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg width="20px" height="20px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path d="M10 12V17" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M14 12V17" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     <path d="M4 7H20" stroke="#ffffff" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
