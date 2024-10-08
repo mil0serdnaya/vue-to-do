9284cfc534c93b2d76f217328f894d794a7663a2
Anya Miloserdnaya <milos3rdnaya@yandex.ru>
2024-10-08 20:36:28 +0500

    header refac

--- src/components/ToDoHeader.vue
+++ src/components/ToDoHeader.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue';
+
 const isFormVisible = ref(false);
 const newToDo = ref({ title: '', description: '' });
 
@@ -8,9 +9,13 @@ const emit = defineEmits(['addToDo']);
 const addNewToDo = () => {
   if (newToDo.value.title.trim()) {
     emit('addToDo', { ...newToDo.value });
-    newToDo.value = { title: '', description: '' };
-    isFormVisible.value = false;
+    resetForm();
   }
+};
+
+const resetForm = () => {
+  newToDo.value = { title: '', description: '' };
+  isFormVisible.value = false;
 };
 </script>
 
@@ -18,22 +23,34 @@ const addNewToDo = () => {
   <header class="to-do-header">
     <div class="to-do-header__top">
       <h1 class="to-do-header__heading">Todo List</h1>
-      <button class="btn--orange" value="new" @click="isFormVisible = !isFormVisible">New</button>
+      <button class="btn--orange" @click="isFormVisible = !isFormVisible">
+        New
+      </button>
     </div>
     <transition name="fade">
       <div v-if="isFormVisible" class="to-do-form">
-        <input class="to-do-input" v-model="newToDo.title" type="text" placeholder="Title" />
-        <input class="to-do-input" v-model="newToDo.description" type="text" placeholder="Description (optional)" />
-        <button class="btn--orange" value="add" @click="addNewToDo">Add</button>
+        <input
+          class="to-do-input"
+          v-model="newToDo.title"
+          type="text"
+          placeholder="Title"
+          required
+        />
+        <input
+          class="to-do-input"
+          v-model="newToDo.description"
+          type="text"
+          placeholder="Description (optional)"
+        />
+        <button class="btn--orange" @click="addNewToDo">Add</button>
       </div>
     </transition>
   </header>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .to-do-header {
   &__top {
-    width: 100%;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -41,8 +58,8 @@ const addNewToDo = () => {
   }
 
   &__heading {
-    font-weight: 700;
     font-size: 28px;
+    font-weight: 700;
   }
 }
 
@@ -53,6 +70,35 @@ const addNewToDo = () => {
 .to-do-input {
   display: block;
   width: 100%;
+  margin-bottom: 10px;
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
   outline: none;
+  font-size: 16px;
+}
+
+.to-do-input:focus {
+  border-color: #4CAF50;
+}
+
+/* .btn--orange {
+  background-color: #FF8469;
+  color: white;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.btn--orange:hover {
+  background-color: #D95133;
+} */
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
 }
 </style>
